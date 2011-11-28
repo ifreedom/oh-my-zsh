f43c29add434821f22ca22b79aa687869a39da7c
Kun Wang <ifreedom.cn@gmail.com>
2011-11-28 07:07:06 -0800

    Fix fixed_nvm_use.

--- custom/lib/fixed_nvm_use.zsh
+++ custom/lib/fixed_nvm_use.zsh
@@ -2,8 +2,9 @@ fixed_nvm_use() {
 	VERSION=$1
 	nvm use $VERSION
     if [[ $NODE_PATH == *$NVM_DIR/*/lib/node_modules* ]]; then
-        NODE_PATH=${NODE_PATH%$NVM_DIR/*/lib/node_modules*}$NVM_DIR/$VERSION/lib/node_modules${PATH#*$NVM_DIR/*/lib/node_modules}
+        NODE_PATH=${NODE_PATH%$NVM_DIR/*/lib/node_modules*}$NVM_DIR/$VERSION/lib/node_modules${NODE_PATH#*$NVM_DIR/*/lib/node_modules}
     else
         NODE_PATH="$NVM_DIR/$VERSION/lib/node_modules:$NODE_PATH"
     fi
+	export NODE_PATH
 }
